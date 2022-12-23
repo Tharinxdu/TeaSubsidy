21ee3df3bb2214d273daeb7bf77859e750201772
Charith Jayarangana <cj.ptsz@yahoo.com>
2022-12-23 22:30:53 +0530

    create headoffice controller and index page for headoffice

--- app/controllers/HeadOffice.php
+++ app/controllers/HeadOffice.php
@@ -4,7 +4,38 @@
 
 class HeadOffice extends Controller
 {
-    //
+    public function index()
+    {
+        if($_SESSION['Role']!="Head Office")
+        {
+            header("Location: ../user/index");
+        }
+        // echo "HeadOffice/index<br>";
+        // echo "<h2>Hello ${_SESSION['Username']}</h2>";
+
+        //view Head office index file
+        $this->view("user/index" , ["Head Office"]);
+       
+    }
+
+    public function Notifications()
+    {
+        if($_SESSION['Role']!="Head Office")
+        {
+            header("Location: ../user/Notifications");
+        }
+        echo "HeadOffice/Notifications<br>";
+    }
+
+    public function FAQ()
+    {
+        if($_SESSION['Role']!="Head Office")
+        {
+            header("Location: ../user/FAQ");
+        }
+        echo "HeadOffice/FAQ<br>";
+    }
+
 }
 
 ?>
