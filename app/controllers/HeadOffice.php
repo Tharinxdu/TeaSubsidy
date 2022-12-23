d0762f4e14877e8aa291aac7bc67f034228b1102
Charith Jayarangana <cj.ptsz@yahoo.com>
2022-12-23 22:34:31 +0530

    create method in headoffice controller to manage regional office page & create view for manage regional office page

--- app/controllers/HeadOffice.php
+++ app/controllers/HeadOffice.php
@@ -36,6 +36,88 @@ class HeadOffice extends Controller
         echo "HeadOffice/FAQ<br>";
     }
 
+    public function manage_regional_offices(){
+        if($_SESSION['Role']!="Head Office")
+        {
+            header("Location: ../user/index");
+        }
+
+        $regionalModel = $this->model("regionModel");
+        $msg = "";
+        // echo "HeadOffice/manage regional offices<br>";
+
+        if(isset($_POST['remove']))
+        {
+            $id = $_POST['remove'];
+            // unset($_POST['remove']);
+            // echo "id = ".$id."<br>";
+            $data = $regionalModel->findById($id);
+
+            if($data == -2)
+            {
+                $msg = "<div class = 'msg red imp'>Error Occured.Try Again or Contact Administrator immediately.</div>";
+            }
+            elseif($data == -1)
+            {
+                $msg = "<div class = 'msg red'>Already Deleted or not Found.</div>";
+            }
+            else
+            {
+                $deleted = $regionalModel->deleteById($id);
+                // if($deleted == -2)
+                // {
+                //     $msg = "<div class='msg red'>Critical Error Occured.Contact Administrator. ASAP.</div>";
+                // }
+                if($deleted == -1)
+                {
+                    $msg = "<div class = 'msg red imp'>Error Occured.Try Again or Contact Administrator.</div>";
+                }
+                elseif($deleted == 1)
+                {
+                    $msg = "<div class = 'msg green'>{$data['region']} Regional Office successfully Deleted.</div>";
+                }
+            }
+            // header("Location:manage_regional_offices");
+        }
+
+        $SearchResults = [];
+        $SearchResults = $regionalModel->find();
+        // print_r($SearchResults);
+        
+        if($SearchResults == -1)
+        {
+            //faild
+            $msg = "<p class='red msg imp'>Database Connection Issue. Please try again Later or contact Administrator.</p>"; 
+        }
+        else
+        {
+            if(isset($_GET['search']))
+            {
+                if(!empty($_GET['search'])){
+                //     $SearchResults = [];
+                //     $msg = "<p class='header'>Please make sure you fill the search bar.</p>";
+                // }
+                // else
+                // {
+                    $SearchResults = $regionalModel->findByNameLike($_GET['search']);
+                    if(empty($SearchResults))
+                    {
+                        $SearchResults = [];
+                        $msg = "<p class='header'>There is no Regional  Offices with name '{$_GET['search']}';</p>";
+                    }
+                    elseif($SearchResults == -1)
+                    {
+                        $SearchResults = [];
+                        $msg = "<div class='red msg imp'>Something went wrong. Try Again or contact your Administrator.</div>";
+                    }
+                }
+                
+            }
+        }
+
+        $this->view("headoffice/manageRegionalOffices" , ['heading'=>"Regional Offices",'data'=>$SearchResults,'msg'=>$msg]);      
+    }
+
 }
 
 ?>
---
+++ app/views/headoffice/manageRegionalOffices.php
@@ -0,0 +1,90 @@
+
+<!-- css -->
+<link rel="stylesheet" href="../css/index.css">
+
+    <h2 class="header"><?= $data['heading'] ?></h2>
+    
+    <div class="search">
+        <form action="" method="GET">
+            <input type="text" name="search" placeholder="Search">
+            <button class="search-btn" type="submit">Search</button>
+            
+            <!-- <img src="" alt="search" srcset=""> -->
+        </form>
+    </div>
+
+    <?php
+    //     if(isset($_POST['remove1']))
+    //     {
+    //         echo "REMOVE!";
+    //         echo $_POST['remove1'];
+    //     }
+    // ?>
+
+    <a href="newregionaloffice"><button class="btn add">Add new Regional Office</button></a>
+
+    <div class="container">
+        <div class="columns gray">
+        Regions
+        </div>
+        <hr class="gray">
+
+        <!-- loop for data given from controller $data[] array -->
+        <?php
+            if(isset($_POST['remove']))
+            {
+                echo $data['msg'];
+                unset($_POST['remove']);
+            }
+            if(empty($data['data']))
+            {
+                if(isset($_POST['SEARCH']))
+                {
+                    echo $data['msg'];
+                    unset($_POST['SEARCH']);
+                }
+                else{ ?>
+                    <p class='header'>There is no Regional Offices. <a href='newregionaloffice'>Add new Regional Offices</a></p>;
+                <?php
+                }
+            }
+            foreach($data['data'] as $row )
+            {
+                ?>
+                <div class="selection-card">
+                    <div>
+                        <?php echo $row['region']; ?>
+                    </div>
+                    <div class="selection-btns">
+                        <form action="viewR" method="POST">  <!-- pass to new controller -->
+                            <input type="hidden" name="id" value="<?php echo $row['userID']; ?>">
+                            <button class="btn" type="submit" name="view">View</button>
+                        </form>
+                        <div class="remove-btn">
+                            <input type="hidden" name="id" value="<?php echo $row['userID']; ?>">
+                            <button class="btn">Remove</button>
+                        </div>
+                        
+                        
+                    </div>
+                
+                </div>
+
+
+        <?php   } 
+        ?>
+
+    </div>
+
+    <div class="pop-up">
+        <div class="pop-up-msg">
+            Are you sure. you want to delete the regional office.
+        </div>
+        <form action="" method="POST">
+            <button class="btn" type="submit" name="remove">Remove</button>
+        </form>
+        <button class="btn">Cancle</button>
+    </div>
+
+
+    <script src="../script/msg.js"></script>
